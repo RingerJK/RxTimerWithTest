8e37e550957f7743dd9ec0f3eb502bc2c5fd5052
Yury Kanetski <ringerjk@gmail.com>
2019-02-25 17:34:31 +0300

    Timer with some tests

---
+++ app/src/main/java/com/ringerjk/rxtimer/Timer.kt
@@ -0,0 +1,77 @@
+package com.ringerjk.rxtimer
+
+import io.reactivex.Observable
+import io.reactivex.subjects.BehaviorSubject
+import java.util.concurrent.TimeUnit
+import java.util.concurrent.atomic.AtomicLong
+
+class TimerImpl : Timer {
+
+    private val timerStateSubject = BehaviorSubject.create<TimerState>()
+
+    private val currentValue = AtomicLong(0)
+    private val offsetValue = AtomicLong(0)
+
+    private enum class TimerState {
+        Start,
+        Stop,
+        Resume,
+        Pause;
+    }
+
+    override fun start() {
+        timerStateSubject.onNext(TimerState.Start)
+    }
+
+    override fun stop() {
+        timerStateSubject.onNext(TimerState.Stop)
+    }
+
+    override fun resume() {
+        timerStateSubject.onNext(TimerState.Resume)
+    }
+
+    override fun pause() {
+        timerStateSubject.onNext(TimerState.Pause)
+    }
+
+    override val timer: Observable<Long> = timerStateSubject.switchMap { state ->
+        when (state) {
+            TimerState.Start -> Observable.interval(0, 1, TimeUnit.SECONDS)
+                .doOnSubscribe {
+                    currentValue.set(0)
+                    offsetValue.set(0)
+                }
+                .doOnNext {
+                    currentValue.set(it)
+                }
+            TimerState.Stop -> {
+                currentValue.set(0)
+                offsetValue.set(0)
+                Observable.empty()
+            }
+            TimerState.Resume -> Observable.interval(0, 1, TimeUnit.SECONDS)
+                .map { offsetValue.get() + it }
+                .doOnNext { currentValue.set(it) }
+
+            TimerState.Pause -> {
+                offsetValue.set(currentValue.get())
+                currentValue.set(0)
+                Observable.empty()
+            }
+        }
+    }
+        .share()
+}
+
+interface Timer {
+    fun start()
+
+    fun stop()
+
+    fun resume()
+
+    fun pause()
+
+    val timer: Observable<Long>
+}
---
+++ app/src/test/java/com/ringerjk/rxtimer/TimerTest.kt
@@ -0,0 +1,126 @@
+package com.ringerjk.rxtimer
+
+import io.reactivex.observers.TestObserver
+import io.reactivex.plugins.RxJavaPlugins
+import io.reactivex.schedulers.TestScheduler
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import java.util.concurrent.TimeUnit
+
+class TimerTest {
+
+    private lateinit var timer: Timer
+    private val testScheduler = TestScheduler()
+
+    @Before
+    fun setup() {
+        timer = TimerImpl()
+
+        RxJavaPlugins.reset()
+
+        RxJavaPlugins.setComputationSchedulerHandler { testScheduler }
+    }
+
+    @After
+    fun tearDown() = RxJavaPlugins.reset()
+
+    private fun advancedByTime(seconds: Long) {
+        testScheduler.advanceTimeBy(seconds, TimeUnit.SECONDS)
+    }
+
+    private fun provideTimerTestObservable(): TestObserver<Long> = timer.timer.test()
+
+    @Test
+    fun start() {
+        val testObs = provideTimerTestObservable()
+
+        timer.start()
+        advancedByTime(5)
+
+        testObs.assertValues(0, 1, 2, 3, 4, 5)
+        testObs.assertNotTerminated()
+    }
+
+    @Test
+    fun startPause() {
+        val testObs = provideTimerTestObservable()
+
+        timer.start()
+        advancedByTime(5)
+        timer.pause()
+        advancedByTime(5)
+
+        testObs.assertValues(0, 1, 2, 3, 4, 5)
+        testObs.assertNotTerminated()
+    }
+
+    @Test
+    fun startPauseResume() {
+        val testObs = provideTimerTestObservable()
+
+        timer.start()
+        advancedByTime(5)
+        timer.pause()
+        advancedByTime(5)
+        timer.resume()
+        advancedByTime(2)
+
+        testObs.assertValues(0, 1, 2, 3, 4, 5, 5, 6, 7)
+        testObs.assertNotTerminated()
+        println(testObs.values())
+    }
+
+    @Test
+    fun startPauseResumeStartPauseResume() {
+        val testObs = provideTimerTestObservable()
+
+        timer.start()
+        advancedByTime(5)
+        timer.pause()
+        advancedByTime(5)
+        timer.resume()
+        advancedByTime(2)
+        timer.start()
+        advancedByTime(2)
+        timer.pause()
+        advancedByTime(2)
+        timer.resume()
+        advancedByTime(2)
+
+        testObs.assertValues(0, 1, 2, 3, 4, 5, 5, 6, 7, 0, 1, 2, 2, 3, 4)
+        testObs.assertNotTerminated()
+        println(testObs.values())
+    }
+
+    @Test
+    fun fewObservables() {
+        val testObs1 = provideTimerTestObservable()
+        val testObs2 = provideTimerTestObservable()
+
+        timer.start()
+        advancedByTime(5)
+        timer.pause()
+
+        testObs1.assertValues(0, 1, 2, 3, 4, 5)
+        testObs2.assertValues(0, 1, 2, 3, 4, 5)
+
+        val testObs3 = provideTimerTestObservable()
+
+        timer.start()
+        advancedByTime(2)
+
+        testObs1.assertValues(0, 1, 2, 3, 4, 5, 0, 1, 2)
+        testObs2.assertValues(0, 1, 2, 3, 4, 5, 0, 1, 2)
+        testObs3.assertValues(0, 1, 2)
+
+        testObs1.assertNotTerminated()
+        testObs2.assertNotTerminated()
+        testObs3.assertNotTerminated()
+
+        println(testObs1.values())
+        println(testObs2.values())
+    }
+
+
+}
